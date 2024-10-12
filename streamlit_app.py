a9e91af9e12626acd267d62d6b9cc14967e141a6
Kabilkumar S <145892913+Ezio17501@users.noreply.github.com>
2024-10-12 13:38:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -127,12 +127,13 @@ def display_candlestick_chart():
         "series": seriesCandlestickChart
     }], 'candlestick')
 
-# Refresh during market hours
-refresh_during_market_hours(refresh_interval=300)  # Set to refresh every 5 minutes
+
 
 # Display candlestick chart
 display_candlestick_chart()
 
+# Refresh during market hours
+refresh_during_market_hours(refresh_interval=300)  # Set to refresh every 5 minutes
 # Sleep to simulate waiting for the next refresh
 time.sleep(1)
 
